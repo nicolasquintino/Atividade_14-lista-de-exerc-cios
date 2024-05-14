3c0ff988d8838f70d7496a0a4044f037d0b4bb90
nicolasquintino <160753713+nicolasquintino@users.noreply.github.com>
2024-05-14 07:32:37 -0300

    Add files via upload

--- Atividade 14/script.js
+++ Atividade 14/script.js
@@ -38,7 +38,7 @@ function exibirResultadosConsole(a, b) {
     console.log("A multiplicação das duas variáveis: " + produto);
 }
 
-// Envio do formulário
+// Envio do form
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
@@ -51,3 +51,32 @@ document.getElementById("form").addEventListener("submit", function(event) {
 
 //*2- Taxa de Desconto*//
 
+function calcularDescontoINSS(salario) {
+    let taxaDesconto, valorDesconto;
+    if (salario <= 1000) {
+        taxaDesconto = 0.08; //Salário até R$ 1.000,00 – desconto de 8%//
+    } else if (salario <= 1500) {
+        taxaDesconto = 0.085; //Salário entre R$ 1.000,01 e 1.500,00 – desconto de 8,5%//
+    } else {
+        taxaDesconto = 0.09; //Salário acima de 1.500,00 – desconto de 9%//
+    }
+    valorDesconto = salario * taxaDesconto;
+    return [taxaDesconto, valorDesconto];
+}
+
+function calcularFolhaPagamento() {
+    const nome = document.getElementById("nome").value;
+    const salario = parseFloat(document.getElementById("salario").value);
+    
+    const [taxaDesconto, valorDesconto] = calcularDescontoINSS(salario);
+    const salarioLiquido = salario - valorDesconto;
+
+    //Mostrar resultado
+
+    document.getElementById("resultadoNome").textContent = nome;
+    document.getElementById("resultadoSalario").textContent = salario.toFixed(2);
+    document.getElementById("resultadoINSS").textContent = valorDesconto.toFixed(2);
+    document.getElementById("resultadoLiquido").textContent = salarioLiquido.toFixed(2);
+
+    document.getElementById("resultado").style.display = "block";
+}
